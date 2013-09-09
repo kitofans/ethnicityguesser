01674904c1358d2afe7009bb8426e199b6a78774
Ankit Kumar <ankitk@stanford.edu>
2013-09-08 21:28:15 -0700

    docs

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 ethnicityguesser
 ================
+
+DOCUMENTATION:
+
+Usage:
+
+-- From the Python interpreter:
+
+Instantiation:
+
+>>> from runner import make_classifier
+>>> mxec = make_classifier()
+
+
+Training: (note that make_classifier already passes in training tokens)
+
+>>> mxec.train()
+
+Classification: (After training)
+>>> mxec.classify('leventhal')
+'jewish'
+>>>mxec.classify('sekhri')
+'indian'
--- runner.py
+++ runner.py
@@ -6,8 +6,15 @@ def unpickle(string):
 	pickle_file = open(string, 'rb')
 	to_ret = pickle.load(pickle_file)
 	return to_ret
-
-
+	
+def make_classifier():
+	all_toks = []
+	indian_toks = unpickle('pickled_names/indian.pkl')
+	jewish_toks = unpickle('pickled_names/jewish.pkl')
+	all_toks.append(indian_toks)
+	all_toks.append(jewish_toks)
+	classifier = mxec(all_toks)
+	return classifier
 
 def main():
 	all_toks = []
@@ -15,7 +22,8 @@ def main():
 	jewish_toks = unpickle('pickled_names/jewish.pkl')
 	all_toks.append(indian_toks)
 	all_toks.append(jewish_toks)
-	return all_toks
+	classifier = mxec(all_toks)
+	classifier.train()
 
 if __name__ == "__main__":
 	main()
